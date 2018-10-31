536dca1108ccf72e0fd09f8706c245401c317f05
mmazzarino <matheus.mazzarino@acad.pucrs.br>
2018-10-31 12:48:44 -0200

    Continuação da implementação do processo de escrita do dosplay

--- Componentes/DISPLAY_LCD_16X2/DISPLAY_LCD_16X2.vhd
+++ Componentes/DISPLAY_LCD_16X2/DISPLAY_LCD_16X2.vhd
@@ -52,7 +52,8 @@ signal clk_configu: STD_LOGIC;
 signal dados_entrada: ARRANJO_TEXTO(0 TO 31);-- := ((x"39"),(x"39"),(x"39"),(x"39"),(x"39"),(x"39"),(x"39"),(x"39"),(x"39"),(x"39"),(x"39"),(x"39"),(x"39"),(x"39"),(x"39"),(x"39"));
 signal enderecos: ARRANJO_TEXTO(0 TO 31) := ((x"84"),(x"85"),(x"86"),(x"87"),(x"88"),(x"89"),(x"8A"),(x"8B"),(x"8C"),(x"8D"),(x"8E"),(x"8F"),(x"90"),(x"91"),(x"92"),(x"93"),
 															(x"C0"),(x"C1"),(x"C2"),(x"C3"),(x"C4"),(x"C5"),(x"C6"),(x"C7"),(x"C8"),(x"C9"),(x"CA"),(x"CB"),(x"CC"),(x"CD"),(x"CE"),(x"CF")
-
+--signal contador_enderecos : INTEGER;
+--signal contador_caracteres : INTEGER;
 ---------------------------------INÍCIO-------------------------------------------
 begin
 
@@ -91,13 +92,6 @@ begin
 				when M => EN <= '0'; ea_ci <= N;
 				when N => EN <= '1'; ea_ci <= O;
 				when O => RS <= '1'; ea_ci <= FEITO;
---				when FEITO => 		
---									case conta2 is
---										when 0 => EN <= '1'; conta2 := conta2 + 1;
---										when 1 => IN_LCD <= dados_entrada(conta1); conta2 := conta2 + 1;
---										when 2 => EN <= '0'; conta1 := conta1 + 1; conta2 := 0;
---									end case;
---									if(conta1 = 32)then conta2 := 0; end if;	
 				when others => NULL; 	
 			end case;		
 		end if;
@@ -105,13 +99,64 @@ begin
 	
 	OPERACOES_BASICAS : process(OUT_PS2, clk_configu)
 	variable conta1: INTEGER RANGE 0 to 31 := 0;
+	variable conta_out_char_lcd: INTEGER RANGE 0 to 31 := 0;
+	variable conta_out_ender_lcd: INTEGER RANGE 0 to 31 := 0;
 	variable conta2: INTEGER RANGE 0 to 2 := 0;
 	variable conta_end: INTEGER RANGE 0 to 31 := 0;
-	variable byte_teclado: STD_LOGIC_VECTOR (7 downto 0);
+	variable byte_teclado: STD_LOGIC_VECTOR (7 downto 0) := ("ZZZZZZZZ");
 	begin
-		byte_teclado <= OUT_PS2;
-			if(clk_configu'event and clk_configu = '1')then	
-				case byte_teclado is
+		byte_teclado := OUT_PS2;
+--		contador_enderecos <= conta_end;
+--		contador_caracteres <= conta1;
+		if(clk_configu'event and clk_configu = '1')then
+			if(ea_ci = FEITO)then
+				case byte_teclado is										
+					when ENT => EN <= '1';--ENTER
+									RS <= '0';
+									IN_LCD <= enderecos(16);
+									EN <= '0';
+									RS <= '1';
+									conta1 := 16;
+									conta_end := 16;
+									byte_teclado := ("ZZZZZZZZ");
+					
+					when TAB => EN <= '1';
+									RS <= '0';
+									IN_LCD <= enderecos(conta1 + 3);
+									EN <= '0';
+									RS <= '1';
+									conta1 := conta1 + 3;
+									conta_end := cont_end + 3;
+									byte_teclado := ("ZZZZZZZZ");
+									
+					when ESC => EN <= '1';
+									RS <= '0';
+									IN_LCD <= (x"00");
+									EN <= '0';
+									RS <= '1';
+									conta1 := 0;
+									conta_end := 0;
+									byte_teclado := ("ZZZZZZZZ");
+									
+					when BSP => EN <= '1';--BACKSPACE
+											RS <= '0';
+											IN_LCD <= enderecos(conta_end - 1);
+											EN <= '0';
+											RS <= '1';
+											entrada_dados(conta1 - 1) <= (x"20");
+											conta_end := conta_end - 1;
+											conta1 := conta1 - 1;
+											byte_teclado := ("ZZZZZZZZ");
+				
+					when "ZZZZZZ" => case conta2 is
+												when 0 => EN <= '1'; conta2 := conta2 + 1;
+												when 1 => IN_LCD <= dados_entrada(conta_out_char_lcd); conta2 := conta2 + 1;
+												when 2 => EN <= '0'; conta_out_char_lcd := conta_out_char_lcd + 1; conta2 := 0;
+											end case;
+											if(conta_out_char_lcd = 32)then 
+												conta2 := 0; 
+											end if;
+					
 					when others => if(conta1 < 31)then
 											dados_entrada(conta1) <= byte_teclado; 
 											conta1 := conta1 + 1; 
@@ -121,44 +166,13 @@ begin
 											dados_entrada(conta1) <= byte_teclado; conta1 := 0; conta_end <= 0;
 											byte_teclado := ("ZZZZZZZZ");
 										end if;
-										
-					when ENTER => EN <= '1';  
-									RS <= '0';
-									IN_LCD <= enderecos(16);
-									EN <= '0';
-									RS <= '1';
-									conta1 := 16;
-									conta_end := 16;
-									byte_teclado := ("ZZZZZZZZ");
-					
-					when TAB =>   EN <= '1';
-									RS <= '0';
-									IN_LCD <= enderecos(conta1 + 3);
-									EN <= '0';
-									RS <= '1';
-									conta1 := conta1 + 3;
-									conta_end := cont_end + 3;
-									byte_teclado := ("ZZZZZZZZ");
-									
-					when ESC =>   EN <= '1';
-									RS <= '0';
-									IN_LCD <= (x"00");
-									EN <= '0';
-									RS <= '1';
-									conta1 := 0;
-									conta_end := 0;
-									byte_teclado := ("ZZZZZZZZ");
-									
-					when BACKSPACE => EN <= '1';
-											RS <= '0';
-											IN_LCD <= enderecos(conta_end - 1);
-											EN <= '0';
-											RS <= '1';
-											entrada_dados(conta1 - 1) <= (x"20");
-											conta_end := conta_end - 1;
-											conta1 := conta1 - 1;
-											byte_teclado := ("ZZZZZZZZ");
-				end case;
-			end if;					  
+				end case;	
+			end if;
+		end if;	
+	end process;
+	
+--	ESCRITA : process(clk_configu, contador_enderecos, contador_caracteres)
+--	begin
+		
 						
 end ARQ_DISPLAY_LCD_16X2;
